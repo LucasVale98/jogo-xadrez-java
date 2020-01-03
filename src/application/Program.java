d1fd13d7c8972663991e840cdd3072a0b36d0255
Lucas Vale <lucasvale1998.lv@gmail.com>
2020-01-03 20:01:00 -0300

    Tratando exceções e limpando a tela

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,6 +6,7 @@ import javax.swing.border.Border;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
@@ -16,16 +17,23 @@ public class Program {
 		ChessMatch chessMatch = new ChessMatch();
 		
 		while (true) {
-			UI.printBoard(chessMatch.getPieces());
-			System.out.println();
-			System.out.println("Print source: ");
-			ChessPosition source = UI.readChessPosition(scanner);
+			try {
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces());
+				System.out.println();
+				System.out.println("Print source: ");
+				ChessPosition source = UI.readChessPosition(scanner);
+				
+				System.out.println();
+				System.out.print("Target: ");
+				ChessPosition target = UI.readChessPosition(scanner);
+				
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);	
+			}catch (ChessException e) {
+				System.out.println(e.getMessage());
+				scanner.nextLine();
+			}
 			
-			System.out.println();
-			System.out.print("Target: ");
-			ChessPosition target = UI.readChessPosition(scanner);
-			
-			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
 		}
 		
 	}
