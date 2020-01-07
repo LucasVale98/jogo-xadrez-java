4445dcf3c60561e3287684de125d961221906344
Lucas Vale <lucasvale1998.lv@gmail.com>
2020-01-07 18:18:55 -0300

    Jogada especial en passant

--- src/application/Program.java
+++ src/application/Program.java
@@ -17,42 +17,39 @@ import chess.ChessPosition;
 
 public class Program {
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
-		List<ChessPiece> captured = new ArrayList<ChessPiece>();
-		
-		while (!chessMatch.getcheckMate()) {
+		List<ChessPiece> captured = new ArrayList<>();
+
+		while (!chessMatch.getCheckMate()) {
 			try {
 				UI.clearScreen();
 				UI.printMatch(chessMatch, captured);
 				System.out.println();
-				System.out.println("Print source: ");
-				ChessPosition source = UI.readChessPosition(scanner);
-				
+				System.out.print("Source: ");
+				ChessPosition source = UI.readChessPosition(sc);
+
 				boolean[][] possibleMoves = chessMatch.possibleMoves(source);
 				UI.clearScreen();
 				UI.printBoard(chessMatch.getPieces(), possibleMoves);
-				
 				System.out.println();
 				System.out.print("Target: ");
-				ChessPosition target = UI.readChessPosition(scanner);
-				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);	
-				
+				ChessPosition target = UI.readChessPosition(sc);
+
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+
 				if (capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
-				
-				
-			}catch (ChessException e) {
+			} catch (ChessException e) {
 				System.out.println(e.getMessage());
-				scanner.nextLine();
-			}catch (InputMismatchException e) {
+				sc.nextLine();
+			} catch (InputMismatchException e) {
 				System.out.println(e.getMessage());
-				scanner.nextLine();
+				sc.nextLine();
 			}
 		}
 		UI.clearScreen();
 		UI.printMatch(chessMatch, captured);
-		
 	}
 }
